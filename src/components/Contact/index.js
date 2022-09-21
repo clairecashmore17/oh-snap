fee9563ac5fad791eceb9035b42729aabf970276
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-21 18:26:58 -0400

    added spa functionality w/ contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
+  //Hook for determining error message
+  const [errorMessage, setErrorMessage] = useState("");
   //add a hook to manage the form data and initialize the input fields to be clear
   const [formState, setFormState] = useState({
     name: "",
@@ -12,9 +15,27 @@ function ContactForm() {
 
   //defining handleChange for user name input
   function handleChange(e) {
+    //validate email, name, or message
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
     //We use ...(spread operator) in order to retain other key-value pairs
     // We can also target all the values with the e.target.name which will take either name, email, or message, making this function versatile
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
 
   //Handle submit function for form
@@ -33,7 +54,7 @@ function ContactForm() {
             type="text"
             name="name"
             defaultValue={name}
-            onChange={handleChange}
+            onBlur={handleChange}
           />
         </div>
         <div>
@@ -42,7 +63,7 @@ function ContactForm() {
             type="email"
             name="email"
             defaultValue={email}
-            onChange={handleChange}
+            onBlur={handleChange}
           />
         </div>
         <div>
@@ -50,9 +71,14 @@ function ContactForm() {
           <textarea
             name="message"
             defaultValue={message}
-            onChange={handleChange}
+            onBlur={handleChange}
             rows="5"
           />
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
         </div>
         <button type="submit">Submit</button>
       </form>
